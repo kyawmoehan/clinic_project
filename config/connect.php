d597f72151451cda34464c485bdff10d0715078b
kyawmoehan <kyawmoehan012@gmail.com>
2023-11-16 17:36:49 +0630

    remove password

--- config/connect.php
+++ config/connect.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $db_name = "clinic_project";
 $username = "root";
-$password = "Kyawmoehan01";
+$password = "";
 try {
   $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
 
